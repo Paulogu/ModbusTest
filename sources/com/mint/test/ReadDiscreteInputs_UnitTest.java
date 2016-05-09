00ade0ae616f41c7c247b5c52ca5c6dfac58e42f
Oliver <oliver.hurtig@aqylon.com>
2016-05-09 12:09:23 +0200

    Test lib Modbus

--- sources/com/mint/test/ReadDiscreteInputs_UnitTest.java
+++ sources/com/mint/test/ReadDiscreteInputs_UnitTest.java
@@ -4,9 +4,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import com.mint.io.modbus.ModbusTCP_Connection;
-import com.mint.io.modbus.functions.ModbusTCP_ReadMultipleRegisters;
+import com.mint.io.modbus.functions.ModbusTCP_ReadDiscreteInputs;
 
-public class ReadMultipleRegisters_UniTest {
+public class ReadDiscreteInputs_UnitTest {
 	public static void main(String[] args) throws IOException {
 		InetAddress ad = null;
 		
@@ -18,13 +18,16 @@ public class ReadMultipleRegisters_UniTest {
 		ModbusTCP_Connection connection= new ModbusTCP_Connection(ad);
 		connection.setUnitId(1);
 		
-		int nbHRegs=4;
+		int nbInputs = 4;
 		
-		ModbusTCP_ReadMultipleRegisters function= new ModbusTCP_ReadMultipleRegisters(0, nbHRegs);
+		
+		// define function
+		ModbusTCP_ReadDiscreteInputs function = new ModbusTCP_ReadDiscreteInputs(0,nbInputs);
+		
 		connection.execute(function);
 		
-		for(int i=0; i<nbHRegs; i++){
-			System.out.print(function.getRegisters(i)+", ");	
-		}
+		for(int i=0; i<nbInputs; i++){
+			System.out.print(function.getInputs(i)+", ");	
+		}				
 	}
 }
